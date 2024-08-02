9aaf68e4388df2d9d8fb9f01b6ca8e79be522de2
vishal <vr473661@gmail.com>
2024-08-02 15:30:23 +0530

    components commit

--- src/configuration/mongo_db_connection.py
+++ src/configuration/mongo_db_connection.py
@@ -1,6 +1,6 @@
 import os
-import sys 
- 
+import sys
+
 import certifi
 import pymongo
 
@@ -11,16 +11,16 @@ ca = certifi.where()
 
 class MongoDBClient:
     client = None
-    
-    def __init__(self,database_name = MONGO_DATABASE_NAME) -> None:
+
+    def __init__(self, database_name=MONGO_DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
                 mongo_db_url = os.getenv("MONGO_DB_URL")
                 if mongo_db_url is None:
-                    raise Exception("Environment key:MONGO_DB_URL is not set.")
-                MongoDBClient.client = pymongo.MongoClient(mongo_db_url,tisCAFile=ca)
+                    raise Exception("Environment key: MONGO_DB_URL is not set.")
+                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
             self.database_name = database_name
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
