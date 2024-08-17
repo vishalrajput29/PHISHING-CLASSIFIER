6eaa74efd48c2ff42bf089361f88238400e1039d
vishal <vr473661@gmail.com>
2024-08-17 20:06:33 +0530

    app commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="vishalrajput",
+    author_email="vr473661@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
